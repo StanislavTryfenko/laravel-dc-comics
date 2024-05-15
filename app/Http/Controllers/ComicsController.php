ce8a222305d29b0aed6c5e95e56d501cbe52584e
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-05-15 17:50:07 +0200

    help struttura

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -12,7 +12,7 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        //
+        return view('comics.index', ['comics'=> Comics::orderByDesc('id')]);
     }
 
     /**
@@ -20,7 +20,7 @@ class ComicsController extends Controller
      */
     public function create()
     {
-        //
+        return view('comics.create');
     }
 
     /**
@@ -28,7 +28,18 @@ class ComicsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $newComic = new Comics();
+        $newComic->title = $data['title'];
+        $newComic->description = $data['description'];
+        $newComic->thumb = $data['thumb'];
+        $newComic->price = $data['price'];
+        $newComic->series = $data['series'];
+        $newComic->sale_date = $data['sale_date'];
+        $newComic->type = $data['type'];
+        $newComic->save();
+        return redirect()->route('comics.index');
     }
 
     /**
@@ -36,7 +47,7 @@ class ComicsController extends Controller
      */
     public function show(Comics $comics)
     {
-        //
+        return view('comics.show', compact('comics'));
     }
 
     /**
--- database/seeders/ComicsSeeder.php
+++ database/seeders/ComicsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comics;
 
 class ComicsSeeder extends Seeder
 {
@@ -12,6 +13,18 @@ class ComicsSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $comics = config('comics.comic');
+
+        foreach ($comics as $comic) {
+            $newComic = new Comics();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+        }
     }
 }
