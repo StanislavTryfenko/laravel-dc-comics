5c7cd3fa9cf2c8a7a86267985bcf3e0b4c13bdb9
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-05-16 10:57:16 +0200

    Add links to the show view

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -8,7 +8,12 @@
             <img src="{{$comic->thumb}}" alt="">
         </div>
         <div class="col">
+            <div class="d-flex justify-content-between"></div>
             <h2>{{$comic->title}}</h2>
+            <div class="actions">
+                <a href="{{route('comics.edit', $comic)}}"><i class="fas fa-pencil-alt fa-sm fa-fw"></i></a>
+                <a href="{{route('comics.index')}}"><i class="fas fa-arrow-left fa-sm fa-fw"></i> Back to pastas</a>
+            </div>
             <p>{{$comic->description}}</p>
             <hr>
             <div class="metadata">
