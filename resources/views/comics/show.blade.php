e4b3dd7a84781e12051d21a0a9995811888bd75e
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-05-16 11:17:00 +0200

    fix cols on comics.show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,10 +4,10 @@
 
 <div class="container py-5">
     <div class="row">
-        <div class="col">
+        <div class="col-2">
             <img src="{{$comic->thumb}}" alt="">
         </div>
-        <div class="col">
+        <div class="col-2">
             <div class="d-flex justify-content-between"></div>
             <h2>{{$comic->title}}</h2>
             <div class="actions">
