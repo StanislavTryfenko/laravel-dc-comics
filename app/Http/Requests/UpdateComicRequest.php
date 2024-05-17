a42ca7fe19968fb72ddb013e0f235bca9843bd82
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-05-17 10:36:13 +0200

    Forcing update without Rule:: but using unique in pipe laravel syntax

--- app/Http/Requests/UpdateComicRequest.php
+++ app/Http/Requests/UpdateComicRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
+//use Illuminate\Validation\Rule;
 
 class UpdateComicRequest extends FormRequest
 {
@@ -24,7 +24,8 @@ class UpdateComicRequest extends FormRequest
     {
         return [
             
-            'title' => ['required', 'min:3', 'max:50', Rule::unique('comics')->ignore($this->comic->id)],
+            //'title' => ['required', 'min:3', 'max:50', Rule::unique('comics')->ignore($this->comic->id)],
+            'title' => 'required|min:3|max:50|unique:comics,title,' . $this->comic->id,
             'description' => 'required|min:3|max:4096',
             'thumb' => 'required|min:3|max:255',
             'price' => 'required|max:10',
